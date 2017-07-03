a1dd9f0c7831944c6228289b34e638f29a697dfb
OPTC-Agenda <ibbus93@gmail.com>
2017-07-03 16:49:57 +0200

    Various updates

--- assets/js/loadJap.js
+++ assets/js/loadJap.js
@@ -234,7 +234,10 @@ function showSpecial(flag){
                         var tiny = specialList[character].tiny;
                         var foo = 'fnModal(\'' + character + '\')';
 
-                        if (character == "Cotton" || character == "CottonEgg" || character == "CottonX3" || character == "Luffy30" || character == "Luffy40" || character == "AceTraining" || character == "RaySocket") {
+                        if (character == "Cotton" || character == "CottonEgg" || character == "CottonX3"
+                            || character == "Luffy30" || character == "Luffy40" || character == "AceTraining"
+                            || character == "RaySocket" || character == "NamiSummer" || character == "RobinSummer"
+                            || character == "PeronaSummer") {
                             $("#list" + (i + 1) + " .special").append("<a href='#viewSpecialModal' onclick='specialModal(\"" + character + "\")' data-toggle='modal'><div style='background-image: url(" + tiny + ")' class='image-div inline'></div></a>");
                         } else {                            
                             if (character == "LvlUp" || character == "LvlUpx3") {
@@ -249,7 +252,7 @@ function showSpecial(flag){
                                     }
                                 }
                             } 
-                            if (character == "Snail" || character == "Sugofest" || character == "FreePull" || character == "Rayleigh") {
+                            if (character == "Snail" || character == "Sugofest" || character == "FreePull" || character == "Rayleigh" || character == "ColaCavern") {
                                 $("#list" + (i + 1) + " .special").append("<div style='background-image: url(" + tiny + ")' class='image-div inline'></div>");
                             }
                         }

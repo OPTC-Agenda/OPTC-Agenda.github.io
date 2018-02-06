a107bc1dbfbd5ec44cca2cd08b55a0659c5585eb
OPTC-Agenda <ibbus93@live.it>
2018-02-06 10:18:08 +0100

    - BOTH: added missing dates - BOTH: added celestial dragons island

--- assets/js/loadJap.js
+++ assets/js/loadJap.js
@@ -235,7 +235,8 @@ function showSpecial(flag){
                         var foo = 'fnModal(\'' + character + '\')';
 
                         if (/^Cotton/.test(character) || /^Luffy/.test(character) || character == "AceTraining" || character == "AceTM"
-                            || character == "RaySocket" || /^Summer/.test(character) || character == "RebeccaJuice" || character == "SanjiLobster" || /^Hime/.test(character) || character == "BuggyTreasure") {
+                            || character == "RaySocket" || /^Summer/.test(character) || character == "RebeccaJuice" || character == "SanjiLobster" || /^Hime/.test(character) || character == "BuggyTreasure" ||
+                            character == "CelestialDragons") {
                             $("#list" + (i + 1) + " .special").append("<a href='#viewSpecialModal' onclick='specialModal(\"" + character + "\")' data-toggle='modal'><div style='background-image: url(" + tiny + ")' class='image-div inline'></div></a>");
                         } else {                            
                             if (/^LvlUp/.test(character)) {
